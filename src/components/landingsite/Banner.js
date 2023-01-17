bb025cf7d591767d275cef470975ac1775678896
Jennygra <83270722+Jennygra@users.noreply.github.com>
2023-01-17 17:38:01 +0100

    Added content to landing site
    Still need styling, and make the component reusable

---
+++ src/components/landingsite/Banner.js
@@ -0,0 +1,14 @@
+import banner from "../../images/landingsite-banner.png";
+
+function Banner() {
+  return (
+    <div>
+      <img
+        src={banner}
+        alt="Share, connect, discover. Dont miss out, register now!"
+      />
+    </div>
+  );
+}
+
+export default Banner;
--- src/components/landingsite/Landingsite.js
+++ src/components/landingsite/Landingsite.js
@@ -1,8 +1,16 @@
+import Banner from "./Banner";
+
 function Landingsite() {
   return (
-    <div>
-      <h1>Landingsite</h1>
-    </div>
+    <>
+      <Banner />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.{" "}
+      </p>
+    </>
   );
 }
 

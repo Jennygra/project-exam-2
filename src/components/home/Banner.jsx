8c9df90dfc00d6b872a23b750a0be1efaa7ccad9
Jennygra <83270722+Jennygra@users.noreply.github.com>
2023-01-24 14:22:13 +0100

    Updated Banner.jsx
    Added dynamic user name

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,13 +1,17 @@
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext/authContext";
 import bannerImg from "../../images/homepage-banner.JPG";
 import Card from "react-bootstrap/Card";
 
 function Banner() {
+  const [auth, setAuth] = useContext(AuthContext);
+
   return (
     <>
       <Card className="bg-dark text-white homepage-banner_container">
         <Card.Img src={bannerImg} alt="Banner img" />
         <Card.ImgOverlay>
-          <Card.Title>Hello (USER)</Card.Title>
+          <Card.Title>Hello, {auth.name}!</Card.Title>
         </Card.ImgOverlay>
       </Card>
     </>

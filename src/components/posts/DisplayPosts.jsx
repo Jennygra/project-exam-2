efc41e7f265b6e379825714b286ea7c4b418f9ae
Jennygra <83270722+Jennygra@users.noreply.github.com>
2023-02-14 07:17:16 +0100

    Clickable cards

--- src/components/posts/DisplayPosts.jsx
+++ src/components/posts/DisplayPosts.jsx
@@ -8,12 +8,12 @@ const DisplayPost = ({ post }) => {
         <div className="posts-item_img-wrapper">
           <img src={checkImg(post.media, img)} alt={post.title} />
         </div>
-      </a>
 
-      <div className="posts-item_details-wrapper">
-        <h4>{post.title}</h4>
-        <p>{post.body}</p>
-      </div>
+        <div className="posts-item_details-wrapper">
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+        </div>
+      </a>
     </div>
   );
 };
